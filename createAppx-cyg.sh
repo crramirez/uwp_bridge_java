b1a581d5f2f03e578494146c5b6b87ffc3f8d3e4
Carlos Rafael Ramirez <crramirez@gmail.com>
2017-06-19 15:32:26 -0500

    Added Cygwin version of the script

---
+++ createAppx-cyg.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+cd "$(dirname "$0")"
+
+tar -xvzf jre-8u*-windows-x64.tar.gz
+mv jre1.8.* PackageFiles/jre
+
+export WINDOWSSDK="/cygdrive/c/Program Files (x86)/Windows Kits/10/bin/10.0.15063.0/x64"
+
+#Make appx
+"${WINDOWSSDK}/makeappx.exe" pack  /d PackageFiles /p SwingSet.appx /o /v /l
+
+#Sign
+"${WINDOWSSDK}/signtool.exe" sign /a /fd SHA256 /f auto-generated.pfx /p 123456  SwingSet.appx
